56c0b1110f4b8fff310ab46ddc121732fda3f7e2
Vardhan Gude <81156630+vardhangude@users.noreply.github.com>
2025-09-11 02:47:41 +0530

    Updated start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+#version=`date + `
+
 #pull the docker image from dockerhub
-docker pull vardhan018/aws-cicd-app
+docker pull vardhan018/aws-cicd-app:latest
 
 #run the docker image in the inter-active terminal
 
-docker run -d 8000:8000  vardhan018/aws-cicd-app
+docker run -d 8000:8000  vardhan018/aws-cicd-app:latest
